a97d7641eb0902e3e2020162c5a2b1bff7581385
Asiel Cabrera <cabrerasiel@gmail.com>
2025-10-21 22:30:07 -0400

    fix: making InputOptions public

--- Sources/Core/Utils/InputOptions.swift
+++ Sources/Core/Utils/InputOptions.swift
@@ -9,9 +9,14 @@
  * Struct for getInput options
  */
 public struct InputOptions {
-  /** Optional. Whether the input is required. If required and not present, will throw. Defaults to false */
-  let required: Bool?
-
-  /** Optional. Whether leading/trailing whitespace will be trimmed for the input. Defaults to true */
-  let trimWhitespace: Bool?
+    /** Optional. Whether the input is required. If required and not present, will throw. Defaults to false */
+    public let required: Bool?
+    
+    /** Optional. Whether leading/trailing whitespace will be trimmed for the input. Defaults to true */
+    public let trimWhitespace: Bool?
+    
+    public init(required: Bool?, trimWhitespace: Bool?) {
+        self.required = required
+        self.trimWhitespace = trimWhitespace
+    }
 }
